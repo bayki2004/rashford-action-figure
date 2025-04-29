383e435fbc4f9bf1a84ee24d3afee424a0c60f33
Kaan Bayraktar <kbayraktar@student.ethz.ch>
2025-04-29 17:44:32 +0200

    please

---
+++ pages/pages.js
@@ -0,0 +1,11 @@
+export default function SuccessPage() {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-8 font-sans text-gray-800">
+        <h1 className="text-4xl font-extrabold text-green-600 mb-6">Thank you for your order!</h1>
+        <p className="text-lg text-center max-w-md">
+          Your action figure is being prepared. You'll hear from us shortly!
+        </p>
+      </div>
+    );
+  }
+  
